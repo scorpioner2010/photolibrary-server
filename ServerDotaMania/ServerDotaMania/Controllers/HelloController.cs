6a074c2ed3ba779339cc12b2e69484527b8a65fa
Taras Samaryn <samarintaras@gmail.com>
2025-03-04 19:03:41 +0800

    added server

---
+++ ServerDotaMania/ServerDotaMania/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/hello")]
+[ApiController]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello from the server!";
+    }
+}
