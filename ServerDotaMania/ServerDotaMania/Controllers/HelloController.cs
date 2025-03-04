2483efdfc9e06a1e7457a7feb53e9ba3397be828
Taras Samaryn <samarintaras@gmail.com>
2025-03-04 19:37:02 +0800

    added

--- ServerDotaMania/ServerDotaMania/Controllers/HelloController.cs
+++ ServerDotaMania/ServerDotaMania/Controllers/HelloController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-[Route("api/hello")]
+[Route("api")]
 [ApiController]
 public class HelloController : ControllerBase
 {
     [HttpGet]
-    public string Get()
+    public IActionResult Get()
     {
-        return "Hello from the server!";
+        return Ok(new { message = "11111122222!" });
     }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] MessageRequest request)
+    {
+        if (request == null || string.IsNullOrEmpty(request.message))
+        {
+            return BadRequest(new { message = "Message is required!" });
+        }
+
+        // Формуємо відповідь залежно від отриманого меседжа
+        string responseMessage = request.message switch
+        {
+            "1" => "111!",
+            "2" => "222!",
+            "3" => "333",
+            _ => "I don't understand this message."
+        };
+
+        return Ok(new { message = responseMessage });
+    }
+}
+
+public class MessageRequest
+{
+    public string message { get; set; }
 }
